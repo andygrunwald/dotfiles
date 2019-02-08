c8ddbe6e99b7ed115761187641660dfa7b05a89c
Andy Grunwald <andygrunwald@gmail.com>
2019-02-08 19:37:30 +0100

    Deactivate killing apps

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -387,21 +387,21 @@ defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Activity Monitor" \
-	"Address Book" \
-	"Calendar" \
-	"cfprefsd" \
-	"Contacts" \
-	"Dock" \
-	"Finder" \
-	"Google Chrome" \
-	"Mail" \
-	"Messages" \
-	"Photos" \
-	"Safari" \
-	"SystemUIServer" \
-	"Terminal" \
-	"iCal"; do
-	killall "${app}" &> /dev/null
-done
+#for app in "Activity Monitor" \
+#	"Address Book" \
+#	"Calendar" \
+#	"cfprefsd" \
+#	"Contacts" \
+#	"Dock" \
+#	"Finder" \
+#	"Google Chrome" \
+#	"Mail" \
+#	"Messages" \
+#	"Photos" \
+#	"Safari" \
+#	"SystemUIServer" \
+#	"Terminal" \
+#	"iCal"; do
+#	killall "${app}" &> /dev/null
+#done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
