420706a7f3363cdb5b492b53e9693c7b85925cf4
Andy Grunwald <andygrunwald@gmail.com>
2020-12-11 18:26:41 +0100

    Updated homebrew installation url

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,15 +10,15 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
 
-  # Install the correct homebrew for each OS type
+  # Install homebrew for macOS
   if test "$(uname)" = "Darwin"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
-
 fi
+
+# Run Homebrew through the Brewfile
+echo "› brew bundle"
+brew bundle
 
 exit 0
