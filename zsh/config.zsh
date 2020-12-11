d3f4064730cdd10553d016812c26b71c107032ed
Andy Grunwald <andygrunwald@gmail.com>
2020-12-11 18:30:49 +0100

    Reworked zsh config

---
+++ zsh/config.zsh
@@ -0,0 +1,10 @@
+setopt EXTENDED_HISTORY # add timestamps to history
+
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
+
+# don't expand aliases _before_ completion has finished
+#   like: git comm-[tab]
+setopt complete_aliases
